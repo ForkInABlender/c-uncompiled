2ab3d3e884d405061b2c326278be6f65388a564c
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2022-12-18 07:43:17 +0000

    Create uncompiled_java_code_as_cplusplus_code.py

---
+++ uncompiled_java_code_as_cplusplus_code.py
@@ -0,0 +1,32 @@
+import cppyy
+
+"""
+
+Please, do not be fooled into thinking this is c/c++ code by default. it is c/c+ code, but a transpiled version of java-code. Please, don't confuse it 
+ for jvm jni code. It isn't and bypasses even needing javac.
+
+Why compile what runs, much less something disguised as something else? 
+
+"""
+
+
+cppyy.cppdef("""
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class m {
+    public:
+        static void main(vector<string> args) {
+    		int i = 0;
+    		for (string a : args) {
+        		cout << i + 1 << " " << a << endl;
+        		i++;
+    		}
+		}
+	//
+};
+""")
+
+cppyy.gbl.m.main(__import__("sys").argv)
